e17e5b62a182eb7944db32f58a2d9cc00a5858cc
Abdulrahman Hamid <abooym@gmail.com>
2025-11-29 04:30:34 -0500

    Lombok not working/ adding constructor

--- Team_Dynamic_Microservice2/src/main/java/com/abdul/Microservice2/Player.java
+++ Team_Dynamic_Microservice2/src/main/java/com/abdul/Microservice2/Player.java
@@ -1,12 +1,5 @@
 package com.abdul.Microservice2;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Player {
     private int playerId;
     private String firstName;
@@ -15,4 +8,79 @@ public class Player {
     private int jerseyNumber;
     private int age;
     private int teamId;
+
+    // empty Constructor
+    public Player() {
+        super();
+		// TODO Auto-generated constructor stub
+    }
+
+    // Constructor with all fields
+    public Player(int playerId, String firstName, String lastName, String position, int jerseyNumber, int age, int teamId) {
+        super();
+        this.playerId = playerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.jerseyNumber = jerseyNumber;
+        this.age = age;
+        this.teamId = teamId;
+    }
+
+    // Getters and Setters
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getJerseyNumber() {
+        return jerseyNumber;
+    }
+
+    public void setJerseyNumber(int jerseyNumber) {
+        this.jerseyNumber = jerseyNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
+    }
 }
--- Team_Dynamic_Microservice2/src/main/java/com/abdul/Microservice2/Team.java
+++ Team_Dynamic_Microservice2/src/main/java/com/abdul/Microservice2/Team.java
@@ -1,16 +1,66 @@
 package com.abdul.Microservice2;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Team {
     private int teamId;
     private String teamName;
-    private String city;      
-    private int founded;
+    private String teamCity;
+    private int teamFounded;
     private String coachName;
+
+    // empty Constructor
+    public Team() {
+        super();
+		// TODO Auto-generated constructor stub
+    }
+
+    // all fields Constructor 
+    public Team(int teamId, String teamName, String teamCity, int teamFounded, String coachName) {
+        super();
+        this.teamId = teamId;
+        this.teamName = teamName;
+        this.teamCity = teamCity;
+        this.teamFounded = teamFounded;
+        this.coachName = coachName;
+    }
+
+    // Getters and Setters
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getTeamCity() {
+        return teamCity;
+    }
+
+    public void setTeamCity(String teamCity) {
+        this.teamCity = teamCity;
+    }
+
+    public int getTeamFounded() {
+        return teamFounded;
+    }
+
+    public void setTeamFounded(int teamFounded) {
+        this.teamFounded = teamFounded;
+    }
+
+    public String getCoachName() {
+        return coachName;
+    }
+
+    public void setCoachName(String coachName) {
+        this.coachName = coachName;
+    }
 }
